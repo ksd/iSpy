c66a4b98b9d6364e2ffe935264df279b1d4ab32a
Kaj Schermer Didriksen <kdidriksen@me.com>
2022-10-13 09:42:01 +0200

    Initial Commit

---
+++ iSpy/iSpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSpyApp.swift
+//  iSpy
+//
+//  Created by ksd on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct iSpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
